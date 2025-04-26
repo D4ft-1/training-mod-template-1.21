d766dd34bcf4f40e3a18a41ca89e2ab1721de28a
Dmetrius <dmetrius.cicchitto6055@gmail.com>
2025-04-26 01:29:26 -0400

    Completed Tutorial

--- src/main/java/com/d4ft/training/item/ModItems.java
+++ src/main/java/com/d4ft/training/item/ModItems.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item SAPPHIRE = registerItem( "sapphire", new Item(new Item.Settings()));
     public static final Item RAW_SAPPHIRE = registerItem( "raw_sapphire", new Item(new Item.Settings()));
-
+ //comment
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TrainingMod.MOD_ID, name), item);
