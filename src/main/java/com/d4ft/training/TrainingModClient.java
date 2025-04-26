31134a939821efc023bad80e335390ff36c050ca
Dmetrius <dmetrius.cicchitto6055@gmail.com>
2025-04-26 01:25:20 -0400

    Completed Tutorial

---
+++ src/main/java/com/d4ft/training/TrainingModClient.java
@@ -0,0 +1,10 @@
+package com.d4ft.training;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrainingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/d4ft/training/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.d4ft.training.item;
+
+import com.d4ft.training.TrainingMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SAPPHIRE = registerItem( "sapphire", new Item(new Item.Settings()));
+    public static final Item RAW_SAPPHIRE = registerItem( "raw_sapphire", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TrainingMod.MOD_ID, name), item);
+
+    }
+
+    public static void registerModItems() {
+        TrainingMod.LOGGER.info("Registering Mod Items for " + TrainingMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+           fabricItemGroupEntries.add(SAPPHIRE);
+           fabricItemGroupEntries.add(RAW_SAPPHIRE);
+
+        });
+    }
+}
+
